87a3a157449299912f1d7ad3acf2783bf4bab3aa
cforsythe <cforsythe.cs@gmail.com>
2018-11-28 06:53:35 -0800

    Can now search by id

--- src/main/java/freedomphones/phonedb/IPhoneRepository.java
+++ src/main/java/freedomphones/phonedb/IPhoneRepository.java
@@ -1,11 +1,15 @@
 package freedomphones.phonedb;
 
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import freedomphones.phonedb.phones.Phone;
 
 @Repository
 public interface IPhoneRepository extends MongoRepository<Phone, String> {
-
+    @Query(value = "{'id':?0}")
+    Optional<Phone> findById(String id);
 }
--- src/main/java/freedomphones/phonedb/PhoneController.java
+++ src/main/java/freedomphones/phonedb/PhoneController.java
@@ -1,9 +1,11 @@
 package freedomphones.phonedb;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import freedomphones.phonedb.phones.Phone;
@@ -17,5 +19,10 @@ public class PhoneController{
     public List<Phone> getAll(){
         List<Phone> phones = phoneRepository.findAll();
         return phones; 
+    }
+    @GetMapping("/findById/{id}")
+    public Optional<Phone> getById(@PathVariable String id){
+        Optional<Phone> phone = phoneRepository.findById(id);
+        return phone;
     }
 }
