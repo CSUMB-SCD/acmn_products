07dfecbe6ad33a467cb4dee5f580b0164f088953
cforsythe <cforsythe@csumb.edu>
2018-12-02 15:50:11 -0800

    added crossorigin

--- src/main/java/freedomphones/phonedb/PhoneController.java
+++ src/main/java/freedomphones/phonedb/PhoneController.java
@@ -3,11 +3,13 @@ package freedomphones.phonedb;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import freedomphones.phonedb.phones.Phone;
 
+@CrossOrigin
 @RestController
 public class PhoneController{
     @Autowired
